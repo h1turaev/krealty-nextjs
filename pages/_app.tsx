5871befa739b418f609e44b375cbece82d74955c
humoyunturaev <h@MacBook-Pro-h.local>
2025-11-14 12:08:08 +0900

    feat: add CustomCursor component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import type { AppProps } from 'next/app';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import React, { useState } from 'react';
-import { light } from '../scss/MaterialTheme';
 import { ApolloProvider } from '@apollo/client';
-import { useApollo } from '../apollo/client';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { appWithTranslation } from 'next-i18next';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { useApollo } from '../apollo/client';
+import CustomCursor from '../libs/components/common/CustomCursor';
 import '../scss/app.scss';
-import '../scss/pc/main.scss';
+import { light } from '../scss/MaterialTheme';
 import '../scss/mobile/main.scss';
+import '../scss/pc/main.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
   // @ts-ignore
@@ -19,6 +20,7 @@ const App = ({ Component, pageProps }: AppProps) => {
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <CustomCursor />
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
